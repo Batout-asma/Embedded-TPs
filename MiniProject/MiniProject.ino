34597adb72d76f34458e53775cad1f1406a814e9
Batoutiva <benramoulasma@gmail.com>
2025-04-29 00:21:58 +0100

    Add servo motor control of front sensor
    Servo motor that roll 180 degree to turn the front sensor make it face the rear when the car is rolling back if its rolling forward again the servo takes the position back again

--- MiniProject/MiniProject.ino
+++ MiniProject/MiniProject.ino
@@ -1,24 +1,31 @@
 #include "BluetoothSerial.h"
 #include <Ultrasonic.h>
+#include <ESP32Servo.h>
 
 BluetoothSerial SerialBT;
 Ultrasonic ultrasonicFront(22, 23);
-Ultrasonic ultrasonicLeft (12, 13);
+Ultrasonic ultrasonicLeft(12, 13);
 Ultrasonic ultrasonicRight(25, 26);
 
-int motor1Pin1 = 2;  // IN1
-int motor1Pin2 = 4;  // IN2
-int enable1Pin = 5;  // ENA
+Servo servoSensor;
+const int servoPin = 27;
+const int servoForwardAngle = 0;
+const int servoBackwardAngle = 180;
+const long rearStopDistanceCm = 7;
 
-int motor2Pin1 = 19; // IN1
-int motor2Pin2 = 21; // IN2
-int enable2Pin = 18; // ENA
+int motor1Pin1 = 2;
+int motor1Pin2 = 4;
+int enable1Pin = 5;
+
+int motor2Pin1 = 19;
+int motor2Pin2 = 21;
+int enable2Pin = 18;
 
 const int freq = 30000;
 const int pwmChannel = 0;
 const int resolution = 8;
-int dutyCycle = 255;
-int backwardDutyCycle = 90;
+int dutyCycle = 200;
+int backwardDutyCycle = 100;
 
 const long frontStopDistanceCm = 10;
 const long sideCheckDelayMs = 200;
@@ -26,9 +33,9 @@ const long turnDurationMs = 750;
 
 bool obstacleDetected = false;
 bool isMovingForward = false;
+bool isMovingBackward = false;
 
 void setup() {
-  // sets the pins as outputs:
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
   pinMode(enable1Pin, OUTPUT);
@@ -37,37 +44,34 @@ void setup() {
   pinMode(motor2Pin2, OUTPUT);
   pinMode(enable2Pin, OUTPUT);
 
-  // configure LEDC PWM
   ledcAttachChannel(enable1Pin, freq, resolution, pwmChannel);
   ledcAttachChannel(enable2Pin, freq, resolution, pwmChannel + 1);
 
   Serial.begin(115200);
   SerialBT.begin("ESP32");
 
-  stopMotors(); // Initialize motors to stop
+  servoSensor.attach(servoPin);
+  servoSensor.write(servoForwardAngle);
+
+  stopMotors();
 }
 
 void loop() {
-  long frontDistanceCm = ultrasonicFront.read(); // Measure the distance with the front sensor
-  Serial.print("Front Distance: ");
-  Serial.print(frontDistanceCm);
-  Serial.println(" cm");
+  long frontDistanceCm = ultrasonicFront.read();
 
   if (frontDistanceCm <= frontStopDistanceCm && isMovingForward && !obstacleDetected) {
+    Serial.println("Obstacle detected while moving forward! Initiating obstacle avoidance.");
     obstacleDetected = true;
     stopMotors();
-    delay(500); // Give time for motors to stop
+    delay(500);
 
     long leftDistanceCm = ultrasonicLeft.read();
     Serial.print("Left Distance: ");
-    Serial.print(leftDistanceCm);
-    Serial.println(" cm");
+    Serial.println(leftDistanceCm);
     delay(sideCheckDelayMs);
-
     long rightDistanceCm = ultrasonicRight.read();
     Serial.print("Right Distance: ");
-    Serial.print(rightDistanceCm);
-    Serial.println(" cm");
+    Serial.println(rightDistanceCm);
 
     if (leftDistanceCm >= rightDistanceCm) {
       Serial.println("Turning Left.");
@@ -75,63 +79,92 @@ void loop() {
       delay(turnDurationMs);
       stopMotors();
       delay(500);
-      moveForward(); // Continue forward after turning
+      moveForward();
     } else {
       Serial.println("Turning Right.");
       TurnRight();
       delay(turnDurationMs);
       stopMotors();
       delay(500);
-      moveForward(); // Continue forward after turning
+      moveForward();
     }
-    obstacleDetected = false; // Reset the obstacle detected flag
-  } else if (frontDistanceCm <= frontStopDistanceCm) {
-    Serial.println("Obstacle detected! Stopping motors.");
-    stopMotors();
-    obstacleDetected = false; // Reset the flag in case it was set previously
-  } else {
-    if (SerialBT.available()) {
-      char command = SerialBT.read();
-      Serial.print("Received command: ");
-      Serial.println(command);
+    obstacleDetected = false;
+    checkObstacleAndStop();
+  }
 
-      switch (command) {
-        case 'F':
-          moveForward();
-          isMovingForward = true; // Set the flag when moving forward
-          obstacleDetected = false;
-          break;
-        case 'B':
-          moveBackward();
-          isMovingForward = false; // Reset the flag
-          obstacleDetected = false;
-          break;
-        case 'S':
-          stopMotors();
-          isMovingForward = false; // Reset the flag
-          obstacleDetected = false;
-          break;
-        case 'L':
-          TurnLeft();
-          delay(1000); // Turn for 1 second
-          stopMotors(); // Stop after turning
-          isMovingForward = false; // Reset the flag
-          obstacleDetected = false;
-          break;
-        case 'R':
-          TurnRight();
-          delay(1000); // Turn for 1 second
-          stopMotors(); // Stop after turning
-          isMovingForward = false; // Reset the flag
-          obstacleDetected = false;
-          break;
-        default:
-          Serial.println("Unknown command!");
-          break;
-      }
+  if (frontDistanceCm <= frontStopDistanceCm && isMovingBackward && !obstacleDetected) {
+    checkObstacleAndStop();
+  }
+
+  if (SerialBT.available()) {
+    char command = SerialBT.read();
+    Serial.print("Received command: ");
+    Serial.println(command);
+    switch (command) {
+      case 'F':
+        Serial.println("Command: F");
+        servoSensor.write(servoForwardAngle);  // Set servo to forward on 'F'
+        delay(100);
+        moveForward();
+        isMovingForward = true;
+        isMovingBackward = false;
+        obstacleDetected = false;
+        break;
+      case 'B':
+        Serial.println("Command: B");
+        servoSensor.write(servoBackwardAngle);  // Set servo to backward on 'B'
+        delay(100);
+        moveBackward();
+        isMovingForward = false;
+        isMovingBackward = true;
+        obstacleDetected = false;
+        break;
+      case 'S':
+        Serial.println("Command: S");
+        stopMotors();
+        isMovingForward = false;
+        isMovingBackward = false;
+        obstacleDetected = false;
+        break;
+      case 'L':
+        Serial.println("Command: L");
+        TurnLeft();
+        delay(1000);
+        stopMotors();
+        isMovingForward = false;
+        isMovingBackward = false;
+        obstacleDetected = false;
+        break;
+      case 'R':
+        Serial.println("Command: R");
+        TurnRight();
+        delay(1000);
+        stopMotors();
+        isMovingForward = false;
+        isMovingBackward = false;
+        obstacleDetected = false;
+        break;
+      default:
+        Serial.println("Unknown command!");
+        break;
     }
   }
-  delay(100); // Small delay for sensor readings and Bluetooth
+  delay(100);
+}
+
+void checkObstacleAndStop() {
+  long frontDistanceCm = ultrasonicFront.read();
+  Serial.print("Distance (in check function): ");
+  Serial.println(frontDistanceCm);
+  if (frontDistanceCm <= frontStopDistanceCm && isMovingForward) {
+    Serial.println("Obstacle detected in front! Stopping motors.");
+    stopMotors();
+    obstacleDetected = true;
+  }else if (frontDistanceCm <= rearStopDistanceCm && isMovingBackward) {
+    Serial.println("Obstacle detected in back! Stopping motors.");
+    stopMotors();
+    obstacleDetected = true;
+  }
 }
 
 void moveForward() {
@@ -170,7 +203,7 @@ void TurnLeft() {
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, LOW);
   digitalWrite(motor2Pin2, HIGH);
-  ledcWrite(enable1Pin, dutyCycle); // Apply speed to the turning motors
+  ledcWrite(enable1Pin, dutyCycle);
   ledcWrite(enable2Pin, dutyCycle);
 }
 
@@ -180,5 +213,6 @@ void TurnRight() {
   digitalWrite(motor1Pin2, HIGH);
   digitalWrite(motor2Pin1, HIGH);
   digitalWrite(motor2Pin2, LOW);
-  ledcWrite(enable1Pin, dutyCycle); // Apply speed to the turning motors
+  ledcWrite(enable1Pin, dutyCycle);
+  ledcWrite(enable2Pin, dutyCycle);
 }
